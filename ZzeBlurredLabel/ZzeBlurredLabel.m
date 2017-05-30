05a4377161408345a10fe1c2c30bf91f26ec6a1a
Victor Cechinel <dev.victorcechinel@gmail.com>
2017-05-30 14:33:39 -0300

    Create method blurryText

--- ZzeBlurredLabel/ZzeBlurredLabel.m
+++ ZzeBlurredLabel/ZzeBlurredLabel.m
@@ -32,4 +32,25 @@
     CGImageRelease(cgimg);
 }
 
+- (void)blurryText {
+    UIGraphicsBeginImageContext(self.bounds.size);
+    [self.layer renderInContext: UIGraphicsGetCurrentContext()];
+    UIImage * image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    CIFilter * blurFilter = [CIFilter filterWithName:@"CIGaussianBlur"];
+    [blurFilter setDefaults];
+    
+    CIImage * imageToBlur = [CIImage imageWithCGImage:image.CGImage];
+    [blurFilter setValue: imageToBlur forKey:kCIInputImageKey];
+    [blurFilter setValue: @(self.blurRadius) forKey:@"inputRadius"];
+    
+    CIImage * outputImage = blurFilter.outputImage;
+    CIContext * context = [CIContext contextWithOptions:nil];
+    CGImageRef cgimg = [context createCGImage:outputImage fromRect:[outputImage extent]];
+    
+    [self.layer setContents:(__bridge id)cgimg];
+    CGImageRelease(cgimg);
+}
+
 @end
