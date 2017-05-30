b5f2b266d53977ded49e0c6da8d07c92f80e3b48
Victor Cechinel <dev.victorcechinel@gmail.com>
2017-05-30 14:57:51 -0300

    Resize image

--- ZzeBlurredLabel/ZzeBlurredLabel.m
+++ ZzeBlurredLabel/ZzeBlurredLabel.m
@@ -10,28 +10,6 @@
 
 @implementation ZzeBlurredLabel
 
-- (void)setBlurryText:(NSString *)text {
-    super.text = text;
-    UIGraphicsBeginImageContext(self.bounds.size);
-    [self.layer renderInContext: UIGraphicsGetCurrentContext()];
-    UIImage * image = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    
-    CIFilter * blurFilter = [CIFilter filterWithName:@"CIGaussianBlur"];
-    [blurFilter setDefaults];
-    
-    CIImage * imageToBlur = [CIImage imageWithCGImage:image.CGImage];
-    [blurFilter setValue: imageToBlur forKey:kCIInputImageKey];
-    [blurFilter setValue: @(self.blurRadius) forKey:@"inputRadius"];
-    
-    CIImage * outputImage = blurFilter.outputImage;
-    CIContext * context = [CIContext contextWithOptions:nil];
-    CGImageRef cgimg = [context createCGImage:outputImage fromRect:[outputImage extent]];
-    
-    [self.layer setContents:(__bridge id)cgimg];
-    CGImageRelease(cgimg);
-}
-
 - (void)blurryText {
     UIGraphicsBeginImageContext(self.bounds.size);
     [self.layer renderInContext: UIGraphicsGetCurrentContext()];
@@ -41,16 +19,34 @@
     CIFilter * blurFilter = [CIFilter filterWithName:@"CIGaussianBlur"];
     [blurFilter setDefaults];
     
-    CIImage * imageToBlur = [CIImage imageWithCGImage:image.CGImage];
-    [blurFilter setValue: imageToBlur forKey:kCIInputImageKey];
+    CIImage * imageToBlur = [CIImage imageWithCGImage: image.CGImage];
+    [blurFilter setValue: imageToBlur forKey: kCIInputImageKey];
     [blurFilter setValue: @(self.blurRadius) forKey:@"inputRadius"];
     
     CIImage * outputImage = blurFilter.outputImage;
-    CIContext * context = [CIContext contextWithOptions:nil];
-    CGImageRef cgimg = [context createCGImage:outputImage fromRect:[outputImage extent]];
+    CIContext * context = [CIContext contextWithOptions: nil];
+    CGImageRef cgimg = [context createCGImage: outputImage fromRect: [outputImage extent]];
     
-    [self.layer setContents:(__bridge id)cgimg];
-    CGImageRelease(cgimg);
+    // Resize
+    CGColorSpaceRef colorspace = CGImageGetColorSpace(cgimg);
+    CGContextRef contextNew = CGBitmapContextCreate(NULL, self.bounds.size.width, self.bounds.size.height,
+                                                 CGImageGetBitsPerComponent(cgimg),
+                                                 CGImageGetBytesPerRow(cgimg),
+                                                 colorspace,
+                                                 CGImageGetAlphaInfo(cgimg));
+    CGColorSpaceRelease(colorspace);
+    
+    if (contextNew == NULL) {
+        [self.layer setContents:(__bridge id)cgimg];
+        CGImageRelease(cgimg);
+    } else {
+        CGContextDrawImage(contextNew, CGRectMake(0, 0, self.bounds.size.width, self.bounds.size.height), cgimg);
+        CGImageRef imgRef = CGBitmapContextCreateImage(contextNew);
+        CGContextRelease(contextNew);
+        [self.layer setContents:(__bridge id)imgRef];
+        CGImageRelease(imgRef);
+    }
+    
 }
 
 @end
